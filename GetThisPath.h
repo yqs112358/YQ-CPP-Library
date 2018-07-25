7e5f1504c50a66c906ab358525760f0314b1d39e
yqs112358 <1425321705@qq.com>
2018-07-25 20:27:45 +0800

    20180725

---
+++ GetThisPath.h
@@ -0,0 +1,24 @@
+#ifndef YQ_GET_THIS_PATH_H
+#define YQ_GET_THIS_PATH_H
+
+#include <string>
+#include <windows.h>
+
+using std::string;
+
+string GetThisPath()
+{
+	char sPath[MAX_PATH+1];
+	if(GetModuleFileName(NULL,(LPSTR)sPath,sizeof(sPath)))
+		return string(sPath);
+	else
+		return "";
+}
+
+string GetThisDir()
+{
+	string sDir(GetThisPath());
+	return strPath.substr(0, sDir.find_last_of('\\', sDir.length())+1);
+}
+
+#endif
---
+++ GetTime.h
@@ -0,0 +1,12 @@
+#ifndef YQ_GET_TIME_H
+#define YQ_GET_TIME_H
+
+#include <ctime>
+#include <string>
+
+using std::string;
+
+
+string GetTime(string *y=nullptr,string *m=nullptr,string *d=nullptr,string *h=nullptr,string *mi=nullptr,string *s=nullptr)
+
+#endif
