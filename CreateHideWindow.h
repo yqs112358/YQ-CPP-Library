1b5625467790974a992ed70190cd73f9fa5c52ad
Qi You <37969157+yqs112358@users.noreply.github.com>
2019-12-25 11:52:42 +0800

    Add files via upload

---
+++ CreateHideWindow.h
@@ -0,0 +1,47 @@
+#include <windows.h>
+
+LRESULT CALLBACK DefWndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+	switch(Message) {
+		case WM_DESTROY: {
+			PostQuitMessage(0);
+			break;
+		}
+		
+		default:
+			return DefWindowProc(hwnd, Message, wParam, lParam);
+	}
+	return 0;
+}
+
+HWND CreateHideWindow(LPCTSTR windowClass, LPCTSTR windowCapital, 
+		WNDPROC windowProc=DefWndProc)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	HINSTANCE hInstance = GetModuleHandle(NULL);
+
+	memset(&wc,0,sizeof(wc));
+	wc.cbSize		 = sizeof(WNDCLASSEX);
+	wc.lpfnWndProc	 = windowProc; 
+	wc.hInstance	 = hInstance;
+	wc.lpszClassName = windowClass;
+
+	ATOM res;
+	if((res = RegisterClassEx(&wc)) == 0)
+		return NULL;
+	
+	hwnd = CreateWindowEx(0,(LPCTSTR)(ULONG_PTR)res,windowCapital
+			,0,CW_USEDEFAULT,CW_USEDEFAULT,0,0,NULL,NULL,hInstance,NULL);
+
+	return hwnd;
+}
+
+void RunMessageProcedure()
+{
+	MSG msg;
+	while (GetMessage(&msg,NULL,0,0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
